b6b39d3460e0d277129feb7ec09317e4cc3065b9
Guy Rotem <guy.rotem@wix.com>
2016-10-10 03:55:59 +0300

    heroku deploy attempt

--- web.js
+++ web.js
@@ -7,12 +7,11 @@ var app = express();
 // app.use(express.logger('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 
-var target = 'http://localhost:9333';
-// var target = 'https://google-translate-proxy.herokuapp.com/';
+// var target = 'http://localhost:9333';
+var target = 'https://google-translate-proxy.herokuapp.com/';
 
 app.get("/api/languages", function (req, res) {
-  req.url = '/languages';
-  proxy.web(req, res, { target: target });
+  res.redirect(target + '/languages');
 });
 
 app.post("/api/translate", function (req, res) {
