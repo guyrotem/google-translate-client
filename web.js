63d4d728dc1a23df6f489527582b63256038f462
Guy Rotem <guy.rotem@wix.com>
2016-10-10 04:19:42 +0300

    add http proxy

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var gzippo = require('gzippo');
 var express = require('express');
-var httpProxy = require('http-proxy');
-var proxy = httpProxy.createProxyServer({toProxy: true});
+// var httpProxy = require('http-proxy');
+// var proxy = httpProxy.createProxyServer({toProxy: true});
 var app = express();
 
 // app.use(express.logger('dev'));
@@ -14,8 +14,8 @@ app.get("/api/languages", function (req, res) {
   res.redirect(target + '/languages');
 });
 
-app.post("/api/translate", function (req, res) {
-  req.url = '/translate';
-  proxy.web(req, res, { target: target });
-});
+// app.post("/api/translate", function (req, res) {
+//   req.url = '/translate';
+//   proxy.web(req, res, { target: target });
+// });
 app.listen(process.env.PORT || 5000);
